bd7c5073061fc6a3a46faf00d96caad4a0f385c5
MaianeLuiza <maianepaixao025@gmail.com>
2025-01-10 17:56:23 -0300

    feat: Implementando a entidade Todo
    git commit --amend --no-edit1

---
+++ src/main/java/com/maianegonzaga/todolist/entity/Todo.java
@@ -0,0 +1,19 @@
+package com.maianegonzaga.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+}
