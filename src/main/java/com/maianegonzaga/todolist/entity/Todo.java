395c69195d388cd64b9fbdcd4ba8c076f65d6019
MaianeLuiza <maianepaixao025@gmail.com>
2025-01-14 17:57:07 -0300

    chore: Adicionando novas propriedades da aplicação

--- src/main/java/com/maianegonzaga/todolist/entity/Todo.java
+++ src/main/java/com/maianegonzaga/todolist/entity/Todo.java
@@ -1,13 +1,9 @@
 package com.maianegonzaga.todolist.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "todos")
-@Getter
-@Setter
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +12,44 @@ public class Todo {
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
 }
